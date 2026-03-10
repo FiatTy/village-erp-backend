02b0bc2984c1dc9d4d8937b71c9e0267f96f9ed7
FiatTy <zxiuy123@gmail.com>
2026-03-10 10:02:36 +0700

    init

---
+++ src/main/java/com/villageserp/villageerpbackend/VillageErpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.villageserp.villageerpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageErpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VillageErpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/villageserp/villageerpbackend/VillageErpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villageserp.villageerpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageErpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
